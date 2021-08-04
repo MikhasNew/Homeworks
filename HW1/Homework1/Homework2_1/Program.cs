a1876caa74a4128d06d7e5d8c79607d52b2aecf7
mikhasN <mikhas.new@gmail.com>
2021-08-04 23:53:35 +0300

    hw2_2 preRelease

--- HW1/Homework1/Homework2_1/Program.cs
+++ HW1/Homework1/Homework2_1/Program.cs
@@ -2,72 +2,73 @@
 
 namespace Homework2_1
 {
-    class Program
+   public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
-                int counter = 0;
-                double sum = 0;
 
-                Console.WriteLine("Enter numbers separating by Enter. Symbol zero stoped your input");
-                string userInput = Console.ReadLine();
-                double parsrUserInput = 0;
+            int counter = 0;
+            double sum = 0;
 
-                while (userInput != "0")
+            Console.WriteLine("Enter numbers separating by Enter. Symbol zero stoped your input");
+            string userInput = Console.ReadLine();
+            double parsrUserInput = 0;
+
+            while (userInput != "0")
+            {
+                if (double.TryParse(userInput.Replace(" ", "").Replace('.', ','), out parsrUserInput))
                 {
-                    if (double.TryParse(userInput.Replace(" ", "").Replace('.', ','), out parsrUserInput))
+                    if (double.IsInfinity(parsrUserInput))
                     {
-                        if (double.IsInfinity(parsrUserInput))
-                        {
-                            PrintWarning("Number contains too many digits");
-                            userInput = Console.ReadLine();
-                            continue;
-                        }
-                            counter++;
-                            sum = sum + parsrUserInput;
-                            Console.WriteLine($"Your input: {parsrUserInput}, enter next number");
-                            userInput = Console.ReadLine();
-                        
-                    }
-                    else
-                    {
-                        PrintWarning(@"Please enter positive and negative, whole or fractional numbers. Example: -23,3 ");
+                        PrintWarning("Number contains too many digits");
                         userInput = Console.ReadLine();
+                        continue;
                     }
+                    counter++;
+                    sum = sum + parsrUserInput;
+                    Console.WriteLine($"Your input: {parsrUserInput}, enter next number");
+                    userInput = Console.ReadLine();
 
-                }
-
-                double average = sum / counter;
-                if (double.IsInfinity(sum))
-                {
-                    PrintWarning("The sum of the numbers entered is too large. Buffer is overflow.");
                 }
                 else
                 {
-                    if (counter > 1)
-                    {
-                        Console.WriteLine($"Your input {counter} numbers. Summ of the numbers = {sum}, average = {average}");
-                    }
-                    else
-                    {
-                        if (counter == 0)
-                            Console.WriteLine($"Your not input number.");
-                        else
-                        Console.WriteLine($"Your input {counter} number. Is number = {sum}");
-                    }
+                    PrintWarning(@"Please enter positive and negative, whole or fractional numbers. Example: -23,3 ");
+                    userInput = Console.ReadLine();
                 }
-            
-            static void PrintWarning(string message)
-            {
-                var conFColor = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(message);
-                Console.ForegroundColor = conFColor;
+
             }
 
+            double average = sum / counter;
+            if (double.IsInfinity(sum))
+            {
+                PrintWarning("The sum of the numbers entered is too large. Buffer is overflow.");
+            }
+            else
+            {
+                if (counter > 1)
+                {
+                    Console.WriteLine($"Your input {counter} numbers. Summ of the numbers = {sum}, average = {average}");
+                }
+                else
+                {
+                    if (counter == 0)
+                        Console.WriteLine($"Your not input number.");
+                    else
+                        Console.WriteLine($"Your input {counter} number. Is number = {sum}");
+                }
+            }
+
+            
 
 
+
+        }
+       public static void PrintWarning(string message)
+        {
+            var conFColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = conFColor;
         }
     }
 }
--- HW1/Homework1/Homework2_2/Program.cs
+++ HW1/Homework1/Homework2_2/Program.cs
@@ -1,12 +1,195 @@
 ﻿using System;
+using Homework2_1;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Homework2_2
-{
+{ 
     class Program
     {
+        #region Console size setting 
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+
+        private static extern IntPtr GetConsoleWindow();
+
+        private static IntPtr ThisConsole = GetConsoleWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int HIDE = 0;
+
+        private const int MAXIMIZE = 3;
+
+        private const int MINIMIZE = 6;
+
+        private const int RESTORE = 9;
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HW_2();
+        }
+        static void HW_2()
+        {
+            double int1 = 0;
+            double int2 = 0;
+
+            double int1ToZero = 0;
+            double int2ToZero = 0;
+
+            Console.WriteLine("Enebled interval of numbers. Enter the first border of the interval.");
+            if (ReadNumbers(Console.ReadLine(), out int1))
+            {
+                Console.WriteLine("Enter second border of the interval.");
+                if (ReadNumbers(Console.ReadLine(), out int2))
+                {
+                    double doubleSumm = 0;
+
+                    if (int1 > int2) //revers interval
+                    {
+                        double temp = int1;
+                        int1 = int2;
+                        int2 = temp;
+                    }
+
+                    Console.WriteLine($"You set the interval: {int1} - {int2}.");
+
+                    //Math.Floor округляет вниз по направлению к отрицательной бесконечности.
+                    //Math.Ceiling округляет вверх по направлению к положительной бесконечности.
+                    //Math.Truncate округляет вниз или вверх по направлению к нулю.
+
+                    //  int1ToZero = decimal.Ceiling(int1);  //first number is always rounded to zero
+                    // int2ToZero = decimal.Floor(int2); //if second number round down
+                    int1ToZero = Math.Ceiling(int1);
+                    int2ToZero = Math.Ceiling(int2);
+
+                    try
+                    {
+                        /////
+                        // Stopwatch stopwatch = new Stopwatch();
+                        // stopwatch.Start();
+
+                        doubleSumm = 0;
+                        double i = 0;
+
+                        if (int1ToZero % 2 != 0)     
+                        {
+                            i = int1ToZero;
+                        }
+                        else
+                            i = int1ToZero + 1;
+
+                        for (; i <= int2ToZero; i += 2)
+                        {
+                            doubleSumm += i;
+                            if(double.IsInfinity(doubleSumm))
+                            {
+                                Homework2_1.Program.PrintWarning(" The sum of the numbers entered is too large. Buffer is overflow.");
+                                break;
+                            }
+
+                        }
+
+                        Console.WriteLine($"Sum of odd numbers within this interval = {doubleSumm}");
+
+                        //  stopwatch.Stop();
+                        // Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+                        // stopwatch = new Stopwatch();
+                        // stopwatch.Start();
+
+                        //doubleSumm = 0;
+                        //for (double ii = int1ToZero; ii <= int2ToZero; ii++)
+                        //{
+                        //    if (ii % 2 != 0)
+                        //    {
+                        //        doubleSumm += ii;
+                        //    }
+                        //}
+
+                        //Console.WriteLine($"Sum of odd numbers within this interval = {doubleSumm}");
+
+                        // stopwatch.Stop();
+                        //смотрим сколько миллисекунд было затрачено на выполнение
+                        //  Console.WriteLine(stopwatch.ElapsedMilliseconds);
+                    }
+                    catch (Exception Ex)
+                    {
+                        Homework2_1.Program.PrintWarning(Ex.Message + " Press any key or enter ESC to exit");
+                        var ConsoleKey = Console.ReadKey();
+                        if (ConsoleKey.Key != System.ConsoleKey.Escape)
+                        {
+                            HW_2();
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+        static bool ReadNumbers(string userInput, out double parsrUserInput)
+        {
+            parsrUserInput = 0;
+            bool iDdouble = false;
+            userInput = userInput.Replace(" ", "").Replace('.', ',');
+
+            if (!double.TryParse(userInput, out parsrUserInput))
+            {
+
+                Homework2_1.Program.PrintWarning(@"Please enter positive and negative, whole or fractional numbers. Example: -23,3. Or enter Ext. ");
+                userInput = Console.ReadLine();
+
+                if (userInput == "Ext")
+                {
+                    iDdouble = false;
+                }
+                else
+                {
+                    iDdouble = ReadNumbers(userInput, out parsrUserInput);
+                }
+            }
+            else
+            {
+
+                if (!IsInfinity(parsrUserInput, true))
+                {
+                    iDdouble = true;
+                }
+                else
+                {
+                    userInput = Console.ReadLine();
+
+                    if (userInput == "Ext")
+                    {
+                        iDdouble = false;
+                    }
+                    else
+                    {
+                        iDdouble = ReadNumbers(userInput, out parsrUserInput);
+                    }
+                }
+                //Console.WriteLine($"Your input: {parsrUserInput}, enter next number");
+
+            }
+
+
+            return iDdouble;
+
+        }
+        static bool IsInfinity(double dicit, bool printErrorString = false)
+        {
+            if (dicit <= double.MaxValue && dicit >= double.MinValue)
+            {
+                return false;
+            }
+            else
+            {
+                if (printErrorString)
+                   Homework2_1.Program.PrintWarning(@"Buffer is overflowed. Enter a other number or enter Ext. ");
+                return true;
+            }
         }
     }
 }
