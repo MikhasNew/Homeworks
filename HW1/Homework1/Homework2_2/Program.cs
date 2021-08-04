0589d845335d65665264b708b6c007ef07973261
mikhasN <mikhas.new@gmail.com>
2021-08-05 00:03:22 +0300

    edit

--- HW1/Homework1/Homework2_2/Program.cs
+++ HW1/Homework1/Homework2_2/Program.cs
@@ -7,25 +7,7 @@ namespace Homework2_2
 { 
     class Program
     {
-        #region Console size setting 
-        [DllImport("kernel32.dll", ExactSpelling = true)]
-
-        private static extern IntPtr GetConsoleWindow();
-
-        private static IntPtr ThisConsole = GetConsoleWindow();
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-
-        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        private const int HIDE = 0;
-
-        private const int MAXIMIZE = 3;
-
-        private const int MINIMIZE = 6;
-
-        private const int RESTORE = 9;
-        #endregion
+       
         static void Main(string[] args)
         {
             HW_2();
